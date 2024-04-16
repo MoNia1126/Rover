44b738e5e7db7d83e14736ff9474811464dc87ee
Monia <moniam1126@gmail.com>
2024-04-16 21:42:08 +0200

    About Page

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:rover/about/about_screen.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "rover",
+      debugShowCheckedModeBanner: false,
+      home: AboutScreen(),
+    );
+  }
+
+}

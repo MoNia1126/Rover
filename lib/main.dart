77f271a3867089cb6dee63f54b9c6896d2a299b3
Monia <moniam1126@gmail.com>
2024-04-20 01:52:30 +0200

    My Trip Page

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: "rover",
       debugShowCheckedModeBanner: false,
-      home: AvailableScreen(),
+      home: MytripScreen(),
     );
   }
 
